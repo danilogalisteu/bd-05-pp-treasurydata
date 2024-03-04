a90830ff0efc867999765ef60c87da8ce6704b14
danilogalisteu <danilogalisteu@gmail.com>
2024-03-03 23:35:06 -0300

    fetching data with puppeteer

--- treasury.js
+++ treasury.js
@@ -1,33 +1,5 @@
 
-async function fetchResponse(url, contentType='text/html', options={}, verbose=false) {
-    try {
-        if (verbose) {
-            console.log(`fetching ${url}...`)
-        }
-        const response = await fetch(url, options)
-        const status = response.status
-        if (status >= 400) {
-            if (verbose) {
-                console.log(`fetch response error on ${url}: ${status}`)
-            }
-            return
-        }
-        const ctype = response.headers.get('content-type')
-        if (!ctype.includes(contentType)) {
-            if (verbose) {
-                console.log(`fetch content error on ${url}: ${ctype}`)
-            }
-            return
-        }
-        return response
-    }
-    catch (err) {
-        if (verbose) {
-            console.log(`fetch error on ${url}: ${err}`)
-        }
-    }
-}
-
+const puppeteer = require("puppeteer")
 
 
 function parseTreasuryTable(content) {
@@ -60,33 +32,43 @@ function parseTreasuryTable(content) {
 
 
 async function getTreasuryTable(verbose=false) {
-    const treasuryTableJSONURL = 'https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json'
-    const response = await fetchResponse(treasuryTableJSONURL, 'application/json', {}, verbose)
-    if (response) {
-        const content = await response.json()
-        const marketDataObj = content['response']['TrsrBondMkt']
-        console.log(`Content:\n${JSON.stringify(marketDataObj, null, 4)}`)
-        const bondDataArray = parseTreasuryTable(content)
-        console.log(`Content:\n${JSON.stringify(bondDataArray, null, 4)}`)
-    }
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    page.setViewport({width: 1600, height: 900})
+
+    const treasuryHomeURL = 'https://www.tesourodireto.com.br/titulos/precos-e-taxas.htm'
+    const treasuryDataURL = `https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json`
+
+    const [dataResponse] = await Promise.all([
+        page.waitForResponse((response) => response.url().startsWith(treasuryDataURL), {timeout: 90_000}),
+        page.goto(treasuryHomeURL, {waitUntil: "domcontentloaded"}),
+    ]);
+
+    const content = await dataResponse.text()
+    await browser.close()
+
+    const dataObj = JSON.parse(content)
+    const marketDataObj = dataObj['response']['TrsrBondMkt']
+    console.log(JSON.stringify(marketDataObj, null, 4))
+    const bondDataArray = parseTreasuryTable(dataObj)
+    console.log(JSON.stringify(bondDataArray, null, 4))
 }
 
 
 function parseTreasuryPriceHistory(content) {
-    const priceHistoryObj = content['response']['TrsrBd']
+    const priceHistoryObj = content['response']['TrsrBd']['PrcgLst']
     const priceHistoryArray = [
         ['code', 'timeOpen', 'timeClose', 'bid', 'ask', 'bidRate', 'askRate'],
     ]
     priceHistoryObj.forEach(
         (element, index, array) => {
             priceHistoryArray.push([
-                element['cd'],
-                element['PrcgLst']['TrsrBdMkt']['opngDtTm'],
-                element['PrcgLst']['TrsrBdMkt']['clsgDtTm'],
-                element['PrcgLst']['untrRedVal'],
-                element['PrcgLst']['untrInvstmtVal'],
-                element['PrcgLst']['anulRedRate'],
-                element['PrcgLst']['anulInvstmtRate'],
+                element['TrsrBdMkt']['opngDtTm'],
+                element['TrsrBdMkt']['clsgDtTm'],
+                element['untrRedVal'],
+                element['untrInvstmtVal'],
+                element['anulRedRate'],
+                element['anulInvstmtRate'],
             ])
         }
     )
@@ -95,13 +77,32 @@ function parseTreasuryPriceHistory(content) {
 
 
 async function getTreasuryPriceHistory(code, period=30, verbose=false) {
-    const treasuryPriceHistoryJSONURL = `https://www.tesourodireto.com.br/b3/tesourodireto/pricesAndFeesHistory?codigo=${code}&periodo=${period}`
-    const response = await fetchResponse(treasuryPriceHistoryJSONURL, 'application/json', {}, verbose)
-    if (response) {
-        const content = await response.json()
-        const priceHistoryArray = parseTreasuryPriceHistory(content)
-        console.log(`Content:\n${JSON.stringify(priceHistoryArray, null, 4)}`)
-    }
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    page.setViewport({width: 1600, height: 900})
+
+    const treasuryHistURL = 'https://www.tesourodireto.com.br/titulos/historico-de-precos-e-taxas.htm'
+    const treasuryDataURL = `https://www.tesourodireto.com.br/b3/tesourodireto/pricesAndFeesHistory`
+
+    await page.goto(treasuryHistURL, {waitUntil: 'load'})
+    await page.click('div.td-form-select-input > div')
+
+    const attr = await page.$$eval("#ul-list > li", el => el.map(x => x.getAttribute("data-codigo")));
+    const items = await page.$$('#ul-list > li')
+    const itemPos = attr.indexOf(code.toString())
+    const itemElement = items[itemPos]
+    itemElement.click()
+
+    const dataResponse = await page.waitForResponse(
+        (response) => response.url().startsWith(treasuryDataURL)
+    )
+
+    const content = await dataResponse.text()
+    await browser.close()
+
+    const dataObj = JSON.parse(content)
+    const priceHistoryArray = parseTreasuryPriceHistory(dataObj)
+    console.log(JSON.stringify(priceHistoryArray, null, 4))
 }
 
 
