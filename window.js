c80433ac7f860250079d96cac7a08511a4855b5c
danilogalisteu <danilogalisteu@gmail.com>
2024-03-05 10:11:27 -0300

    added box for bond data, updated from selected table row

--- window.js
+++ window.js
@@ -4,35 +4,60 @@ const contrib = require('blessed-contrib')
 const { getTreasuryTable, getTreasuryPriceHistory } = require('./treasury.js')
 
 
-async function updateTreasuryData(boxMarket, tableBonds) {
-    const [marketDataObj, bondDataArray] = await getTreasuryTable(true)
+let screen = {}
+let marketDataObj = {}
+let bondDataDF = {}
+let tableBonds = {}
+let boxBonds = {}
+let boxMarket = {}
 
-    colorMarket = marketDataObj['status'] == 'Aberto' ? 'green' : 'orange'
+async function updateTreasuryData() {
+    [marketDataObj, bondDataDF] = await getTreasuryTable(true)
+
+    colorMarket = marketDataObj['status'] == 'Aberto' ? 'green' : 'red'
     stringMarket =
-        `Status  {${colorMarket}-fg}${marketDataObj['status']} [${marketDataObj['statusCode']}]{/${colorMarket}-fg}` + '\n' +
-        `Current {${colorMarket}-fg}${marketDataObj['quoteTime']}{/${colorMarket}-fg}` + '\n' +
-        `Open    ${marketDataObj['openTime']}` + '\n' +
-        `Close   ${marketDataObj['closeTime']}`
+        `Status {${colorMarket}-fg}${marketDataObj['status']} [${marketDataObj['statusCode']}]{/${colorMarket}-fg}` + '\n' +
+        `Last   ${marketDataObj['quoteTime']}` + '\n' +
+        `Open   ${marketDataObj['openTime']}` + '\n' +
+        `Close  ${marketDataObj['closeTime']}`
     boxMarket.setContent(stringMarket)
 
+    const tableDataDF = bondDataDF.loc({columns: [
+        'Index', 'Type', 'Maturity', 'Coupon', 'Name'
+    ]})
     tableBonds.setData({
-        headers: bondDataArray.slice(0, 1)[0].slice(0, -3),
-        data: bondDataArray.slice(1, -1).map((el) => el.slice(0, -3)),
+        headers: tableDataDF.columns,
+        data: tableDataDF.values,
     })
+}
+
+async function updateTreasuryInfo() {
+    const indexTable = tableBonds.rows.selected
+    const infoBond = bondDataDF.iloc({rows: [indexTable]})
+    const stringBond =
+        `Index      ${indexTable}` + '\n' +
+        `Sel        ${tableBonds.rows.selected}` + '\n' +
+        `Total      ${tableBonds.rows.items.length}` + '\n\n' +
+        `Bond       ${infoBond['Type'].values[0]} ${infoBond['Maturity'].values[0]}` + '\n' +
+        `Code       ${infoBond['Code'].values[0]}` + '\n' +
+        `ISIN       ${infoBond['ISIN'].values[0]}` + '\n\n' +
+        `Bid Price  ${infoBond['Bid Price'].values[0]}` + '\n' +
+        `Ask Price  ${infoBond['Ask Price'].values[0]}` + '\n' +
+        `Bid Rate   ${infoBond['Bid Rate'].values[0]}` + '\n' +
+        `Ask Rate   ${infoBond['Ask Rate'].values[0]}` + '\n\n' +
+        `Info       ${infoBond['Info'].values[0]}` + '\n\n' +
+        `Objective  ${infoBond['Objective'].values[0]}` + '\n\n' +
+        `Income     ${infoBond['Income'].values[0]}`
+    boxBonds.setContent(stringBond)
+    screen.render()
 }
 
 
 async function showMainWindow() {
-    const screen = blessed.screen()
+    screen = blessed.screen()
     screen.program.clear()
     const grid = new contrib.grid({rows: 100, cols: 100, screen: screen})
 
-    const paramsBoxMarket = {
-        label: 'Treasury Market',
-        tags: true,
-        border: {type: 'line', fg: "cyan"},
-        fg: 'white',
-      }
     const paramsTableBonds = {
         keys: true,
         vi: true,
@@ -43,21 +68,42 @@ async function showMainWindow() {
         label: 'Treasury Bonds',
         border: {type: "line", fg: "cyan"},
         columnSpacing: 3,
-        columnWidth: [10, 10, 45, 12, 6, 10, 10, 10, 10, 15, 5],
+        columnWidth: [10, 10, 12, 6, 45],
+    }
+    const paramsBoxBonds = {
+        label: 'Bond Info',
+        tags: true,
+        border: {type: 'line', fg: "cyan"},
+        fg: 'white',
+    }
+    const paramsBoxMarket = {
+        label: 'Treasury Market',
+        tags: true,
+        border: {type: 'line', fg: "cyan"},
+        fg: 'white',
     }
 
-    const boxMarket = grid.set(85, 80, 17, 20, blessed.box, paramsBoxMarket)
-    const tableBonds = grid.set(0, 0, 100, 80, contrib.table, paramsTableBonds)
+    tableBonds = grid.set(0, 0, 100, 70, contrib.table, paramsTableBonds)
+    boxBonds = grid.set(0, 70, 85, 30, blessed.box, paramsBoxBonds)
+    boxMarket = grid.set(85, 70, 17, 30, blessed.box, paramsBoxMarket)
 
-    screen.append(boxMarket)
     screen.append(tableBonds)
+    screen.append(boxBonds)
+    screen.append(boxMarket)
     tableBonds.focus()
+
+    tableBonds.rows.on('keypress', async function(ch, key) {
+        if ((key.name == 'down') || (key.name == 'up')) {
+            await updateTreasuryInfo()
+        }
+    });
 
     screen.key(['escape', 'q', 'C-c'], function(ch, key) {
         return process.exit(0);
     });
 
-    await updateTreasuryData(boxMarket, tableBonds)
+    await updateTreasuryData()
+    await updateTreasuryInfo(0)
 
     screen.render()
 }
